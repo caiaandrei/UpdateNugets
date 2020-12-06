c3ec57fe0a3e66787aedc2a465d6a3514a961a8b
Andrei Caia <caiaandrei14@hotmail.com>
2020-12-06 15:55:27 +0200

    Update Nugets

---
+++ UpdateNugets.View/Helpers/InvertBooleanToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UpdateNugets.UI.Helpers
+{
+    public sealed class InvertBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = false;
+
+            if (value is bool)
+            {
+                bValue = (bool)value;
+            }
+
+            else if (value is Nullable<bool>)
+            {
+                Nullable<bool> tmp = (Nullable<bool>)value;
+                bValue = tmp.HasValue ? tmp.Value : false;
+            }
+
+            return (bValue) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
